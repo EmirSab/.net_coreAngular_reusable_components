df04aa63f1960ad78000dde5e4c4b894f72919dc
Emir Šabanić <emir.sabanic.ro@gmail.com>
2022-03-25 21:59:43 +0100

    Adding pagination for users

--- client/src/app/_services/account.service.ts
+++ client/src/app/_services/account.service.ts
@@ -27,15 +27,14 @@ export class AccountService {
 
   register(model: any) {
     return this.http.post(this.baseUrl + 'account/register', model).pipe(
-      map((response: User) => {
-        const user = response;
-        if(user) {
-          this.setCurrentUser(user);
+      map((user: User) => {
+        if (user) {
+         this.setCurrentUser(user);
         }
-        //return user;
       })
     )
   }
+
 
   setCurrentUser(user: User) {
     localStorage.setItem('user', JSON.stringify(user));
